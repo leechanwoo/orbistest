050cc7677fbdb9c4930619b32b4284fc978dff54
chanwoo <chanwoo@chanwooui-MacBook-Pro.local>
2018-03-13 15:07:19 +0900

    set tar file download link

--- tfclient_package/setup.py
+++ tfclient_package/setup.py
@@ -7,7 +7,7 @@ setup(
   author = 'Chanwoo Lee',
   author_email = 'cjswosa2@gmail.com',
   url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/leechanwoo/orbistest/blob/master/tfclient_package/package.tar', # I'll explain this in a second
   keywords = ['testing', 'logging', 'example'], # arbitrary keywords
   classifiers = [],
 )
