808618108cbdca45a59111be4650aea47d3effdb
chanwoo <chanwoo@chanwooui-MacBook-Pro.local>
2018-03-13 15:05:51 +0900

    create tar file

--- tfclient_package/setup.py
+++ tfclient_package/setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 setup(
-  name = 'mypackage',
-  packages = ['mypackage'], # this must be the same as the name above
+  name = 'tfclient_package',
+  packages = ['tfclient_package'], # this must be the same as the name above
   version = '0.1',
   description = 'A random test lib',
-  author = 'Peter Downs',
-  author_email = 'peterldowns@gmail.com',
+  author = 'Chanwoo Lee',
+  author_email = 'cjswosa2@gmail.com',
   url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
   download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
   keywords = ['testing', 'logging', 'example'], # arbitrary keywords
